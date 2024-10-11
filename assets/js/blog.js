bf10d3f0e546acffae483d3b6212d2967732be9e
Brian Solano <brian_solano110012@yahoo.com>
2024-10-11 18:49:43 -0500

    Added javascript to store and read localStorage

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,9 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
+const mainElement = document.querySelector('main');
+const backButton = document.getElementById('back');
 
 // TODO: Create a function that builds an element and appends it to the DOM
+
 
 // TODO: Create a function that handles the case where there are no blog posts to display
 
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,14 +12,32 @@ toggleButton.addEventListener('click', () => {
 });
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
+function readLocalStorage(key) {
+  const data = localStorage.getItem(key);
+  if (data === null) {
+      return [];
+  }
+  try {
+      return JSON.parse(data);
+  } catch (error) {
+      console.error("Error parsing localStorage data:", error);
+      return [];
+  }
+}
 
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
+function storeLocalStorage(key, newData) {
+  const existingData = readLocalStorage(key);
+  existingData.push(newData);
+  localStorage.setItem(key, JSON.stringify(existingData));
+}
+
 
 
 // ! Use the following function whenever you need to redirect to a different page
 
-let redirectURL = '';
+let redirectURL = 'index.html';
 
 const redirectPage = function (url) {
   redirectURL = url;
